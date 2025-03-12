da15b02af2a06c390769a1b75275235e696ebee7
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-03-12 17:27:07 +0100

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/navbar/navBar";
 import { useState } from "react";
 import Footer from "./components/footer/footer";
+import Header from "./components/header/Header";
 
 export default function Home() {
 	const [user, setUser] = useState(false);
 	return (
 		<>
 			<NavBar setUser={setUser} user={user} />
+			<Header />
 			<Footer />
 		</>
 	);
